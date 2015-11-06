3a74d17a3b3d8af7734ceee1ff8cc52896a5c675
Sébastien Muller <sebnmuller@gmail.com>
2015-11-06 09:49:00 +0100

    Added responses for different paths

--- section2/lecture1/server.js
+++ section2/lecture1/server.js
@@ -4,8 +4,22 @@ var http = require('http'),
 
 server.on('request', function(request, response) {
 	console.log("--incoming request--");
-	response.writeHead(200, {'Contet-Type':'text/plain'});
-	response.end('Hello World');
+	var incomingUrl = url.parse(request.url);
+	console.log(incomingUrl);
+	
+	if(incomingUrl.path === '/hello') {
+		response.writeHead(200, {'Contet-Type':'text/plain'});
+		response.end('Hello World');	
+	} else if (incomingUrl.path === '/goodbye') {
+		response.writeHead(200, {'Contet-Type':'text/plain'});
+		response.end('Goodbye World');
+	} else {
+		response.writeHead(404, {'Contet-Type':'text/plain'});
+		response.end('Resource not found');
+	}
+	
+	//response.writeHead(200, {'Contet-Type':'text/plain'});
+	//response.end('Hello World');
 });
 
 server.listen(9000);
